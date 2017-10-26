56361a985df9391a9d920791b11bb81bb36e00df
Adam Yang <ymmsolo@gmail.com>
2017-10-27 07:28:26 +0800

    Update logRegres.py

--- Ch05/logRegres.py
+++ Ch05/logRegres.py
@@ -3,7 +3,7 @@ Created on Oct 27, 2010
 Logistic Regression Working Module
 @author: Peter
 '''
-from numpy import *
+import numpy as np
 
 def loadDataSet():
     dataMat = []; labelMat = []
@@ -12,18 +12,18 @@ def loadDataSet():
         lineArr = line.strip().split()
         dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
         labelMat.append(int(lineArr[2]))
-    return dataMat,labelMat
+    return dataMat, labelMat
 
 def sigmoid(inX):
-    return 1.0/(1+exp(-inX))
+    return 1.0 / (1 + np.exp(-inX))
 
 def gradAscent(dataMatIn, classLabels):
-    dataMatrix = mat(dataMatIn)             #convert to NumPy matrix
-    labelMat = mat(classLabels).transpose() #convert to NumPy matrix
-    m,n = shape(dataMatrix)
+    dataMatrix = np.mat(dataMatIn)             #convert to NumPy matrix
+    labelMat = np.mat(classLabels).transpose() #convert to NumPy matrix
+    m, n = np.shape(dataMatrix)
     alpha = 0.001
     maxCycles = 500
-    weights = ones((n,1))
+    weights = np.ones((n, 1))
     for k in range(maxCycles):              #heavy on matrix operations
         h = sigmoid(dataMatrix*weights)     #matrix mult
         error = (labelMat - h)              #vector subtraction
@@ -32,30 +32,30 @@ def gradAscent(dataMatIn, classLabels):
 
 def plotBestFit(weights):
     import matplotlib.pyplot as plt
-    dataMat,labelMat=loadDataSet()
-    dataArr = array(dataMat)
-    n = shape(dataArr)[0] 
+    dataMat, labelMat = loadDataSet()
+    dataArr = np.array(dataMat)
+    n = np.shape(dataArr)[0]
     xcord1 = []; ycord1 = []
     xcord2 = []; ycord2 = []
     for i in range(n):
-        if int(labelMat[i])== 1:
-            xcord1.append(dataArr[i,1]); ycord1.append(dataArr[i,2])
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i, 1]); ycord1.append(dataArr[i, 2])
         else:
-            xcord2.append(dataArr[i,1]); ycord2.append(dataArr[i,2])
+            xcord2.append(dataArr[i, 1]); ycord2.append(dataArr[i, 2])
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
     ax.scatter(xcord2, ycord2, s=30, c='green')
-    x = arange(-3.0, 3.0, 0.1)
+    x = np.arange(-3.0, 3.0, 0.1)
     y = (-weights[0]-weights[1]*x)/weights[2]
     ax.plot(x, y)
-    plt.xlabel('X1'); plt.ylabel('X2');
+    plt.xlabel('X1'); plt.ylabel('X2')
     plt.show()
 
 def stocGradAscent0(dataMatrix, classLabels):
-    m,n = shape(dataMatrix)
+    m, n = np.shape(dataMatrix)
     alpha = 0.01
-    weights = ones(n)   #initialize to all ones
+    weights = np.ones(n)   #initialize to all ones
     for i in range(m):
         h = sigmoid(sum(dataMatrix[i]*weights))
         error = classLabels[i] - h
@@ -63,13 +63,13 @@ def stocGradAscent0(dataMatrix, classLabels):
     return weights
 
 def stocGradAscent1(dataMatrix, classLabels, numIter=150):
-    m,n = shape(dataMatrix)
-    weights = ones(n)   #initialize to all ones
+    m, n = np.shape(dataMatrix)
+    weights = np.ones(n)   #initialize to all ones
     for j in range(numIter):
-        dataIndex = range(m)
+        dataIndex = list(range(m))
         for i in range(m):
-            alpha = 4/(1.0+j+i)+0.0001    #apha decreases with iteration, does not 
-            randIndex = int(random.uniform(0,len(dataIndex)))#go to 0 because of the constant
+            alpha = 4/(1.0+j+i)+0.0001    #apha decreases with iteration, does not
+            randIndex = int(np.random.uniform(0, len(dataIndex)))#go to 0 because of the constant
             h = sigmoid(sum(dataMatrix[randIndex]*weights))
             error = classLabels[randIndex] - h
             weights = weights + alpha * error * dataMatrix[randIndex]
@@ -86,28 +86,28 @@ def colicTest():
     trainingSet = []; trainingLabels = []
     for line in frTrain.readlines():
         currLine = line.strip().split('\t')
-        lineArr =[]
+        lineArr = []
         for i in range(21):
             lineArr.append(float(currLine[i]))
         trainingSet.append(lineArr)
         trainingLabels.append(float(currLine[21]))
-    trainWeights = stocGradAscent1(array(trainingSet), trainingLabels, 1000)
+    trainWeights = stocGradAscent1(np.array(trainingSet), trainingLabels, 1000)
     errorCount = 0; numTestVec = 0.0
     for line in frTest.readlines():
         numTestVec += 1.0
         currLine = line.strip().split('\t')
-        lineArr =[]
+        lineArr = []
         for i in range(21):
             lineArr.append(float(currLine[i]))
-        if int(classifyVector(array(lineArr), trainWeights))!= int(currLine[21]):
+        if int(classifyVector(np.array(lineArr), trainWeights)) != int(currLine[21]):
             errorCount += 1
     errorRate = (float(errorCount)/numTestVec)
-    print "the error rate of this test is: %f" % errorRate
+    print("the error rate of this test is: %f" % errorRate)
     return errorRate
 
 def multiTest():
-    numTests = 10; errorSum=0.0
+    numTests = 10; errorSum = 0.0
     for k in range(numTests):
         errorSum += colicTest()
-    print "after %d iterations the average error rate is: %f" % (numTests, errorSum/float(numTests))
+    print("after %d iterations the average error rate is: %f" % (numTests, errorSum/float(numTests)))
         
