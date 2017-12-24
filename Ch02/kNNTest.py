ca7c2822f11cf316de18edbc722b0f7aec599c55
hui <zhanghuizaizhe@hotmail.com>
2017-12-24 22:23:45 +0800

    add kNNTest

--- Ch02/kNNTest.py
+++ Ch02/kNNTest.py
@@ -6,14 +6,18 @@ Created on Oct 27, 2010
 from numpy import *
 import matplotlib
 import matplotlib.pyplot as plt
+
 from Ch02 import kNN
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-datingDataMat,datingLabels = kNN.file2matrix('../datingTestSet.txt')
-#ax.scatter(datingDataMat[:,1], datingDataMat[:,2])
+datingDataMat,datingLabels = kNN.file2matrix('datingTestSet.txt')
+# ax.scatter(datingDataMat[:,1], datingDataMat[:,2])
 ax.scatter(datingDataMat[:,1], datingDataMat[:,2], 15.0*array(datingLabels), 15.0*array(datingLabels))
 ax.axis([-2,25,-0.2,2.0])
 plt.xlabel('Percentage of Time Spent Playing Video Games')
 plt.ylabel('Liters of Ice Cream Consumed Per Week')
 plt.show()
+
+#Test dating class
+# kNN.datingClassTest()
